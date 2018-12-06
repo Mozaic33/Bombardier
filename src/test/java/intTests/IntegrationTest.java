171c82f4708a5a1bdde70b100b289455e2782b95
Dumitru Neacsu <Dumitru.Neacsu@endava.com>
2018-12-06 14:49:17 +0200

    chrome

--- src/test/java/intTests/IntegrationTest.java
+++ src/test/java/intTests/IntegrationTest.java
@@ -6,8 +6,11 @@ import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.phantomjs.PhantomJSDriver;
 import org.testng.Assert;
 import org.testng.TestRunner;
-
+import org.openqa.selenium.chrome.ChromeDriver;
 import java.io.File;
+
+import static org.openqa.grid.common.SeleniumProtocol.Selenium;
+
 @Category(IntegrationTest.class)
 
 public class IntegrationTest {
@@ -15,9 +18,9 @@ public class IntegrationTest {
 
     public void TestTitle1() {
 
-        File src = new File("/usr/local/bin/phantomjs");
-        System.setProperty("phantomjs.binary.path", src.getAbsolutePath());
-        WebDriver driver = new PhantomJSDriver();
+        File src = new File("/usr/bin/google-chrome");
+        System.setProperty("webdriver.chrome.driver", src.getAbsolutePath());
+        WebDriver driver = new ChromeDriver();
         driver.get("http://192.168.11.87:8989/AMGrads");
         String title1 = driver.getTitle();
         Assert.assertTrue(title1.contains("MyApp"));
